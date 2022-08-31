fc3030be4bc72c0a6f740d891c730b0c34207aa8
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-08-31 10:09:07 -0500

    added list-all key value pair to coffee object

--- main.js
+++ main.js
@@ -2,20 +2,20 @@
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 let coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
+    {id: 1, name: 'Light City', roast: 'light', list: 'all'},
+    {id: 2, name: 'Half City', roast: 'light', list: 'all'},
+    {id: 3, name: 'Cinnamon', roast: 'light', list: 'all'},
+    {id: 4, name: 'City', roast: 'medium', list: 'all'},
+    {id: 5, name: 'American', roast: 'medium', list: 'all'},
+    {id: 6, name: 'Breakfast', roast: 'medium', list: 'all'},
+    {id: 7, name: 'High', roast: 'dark', list: 'all'},
+    {id: 8, name: 'Continental', roast: 'dark', list: 'all'},
+    {id: 9, name: 'New Orleans', roast: 'dark', list: 'all'},
+    {id: 10, name: 'European', roast: 'dark', list: 'all'},
+    {id: 11, name: 'Espresso', roast: 'dark', list: 'all'},
+    {id: 12, name: 'Viennese', roast: 'dark', list: 'all'},
+    {id: 13, name: 'Italian', roast: 'dark', list: 'all'},
+    {id: 14, name: 'French', roast: 'dark', list: 'all'},
 ];
 
 // Variables
@@ -54,6 +54,9 @@ function updateCoffees(e) {
     let filteredCoffees = [];
     coffees.forEach(function(coffee) {
         if (coffee.roast === selectedRoast) {
+            filteredCoffees.push(coffee);
+
+        } else if (coffee.completeList === selectedRoast) {
             filteredCoffees.push(coffee);
         }
     });
